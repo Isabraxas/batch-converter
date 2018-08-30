41e3a2c6b9dd37e7fc2349d421c846ad9f0e39c0
Isabraxas <isvar.vega@gmail.com>
2018-08-30 14:09:21 -0400

    clean project

--- src/main/java/cc/viridian/servicebatchconverter/persistence/Send.java
+++
@@ -1,9 +0,0 @@
-package cc.viridian.servicebatchconverter.persistence;
-
-import cc.viridian.servicebatchconverter.persistence.auto._Send;
-
-public class Send extends _Send {
-
-    private static final long serialVersionUID = 1L; 
-
-}
